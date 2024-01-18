2b972bc7c5331734518e91a6ba55fb3a2b412a8c
Mikedromeda <82773161+Mikedromeda@users.noreply.github.com>
2024-01-19 00:16:25 +0700

    add button 3

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from config import (
     API_HASH,
     APP_ID,
     CHANNEL_ID,
-    FORCE_SUB_CHANNEL1,
+    FORCE_SUB_CHANNEL,
     FORCE_SUB_CHANNEL2,
     FORCE_SUB_GROUP,
     LOGGER,
@@ -49,24 +49,24 @@ class Bot(Client):
             )
             sys.exit()
 
-        if FORCE_SUB_CHANNEL1:
+        if FORCE_SUB_CHANNEL:
             try:
-                info = await self.get_chat(FORCE_SUB_CHANNEL1)
+                info = await self.get_chat(FORCE_SUB_CHANNEL)
                 link = info.invite_link
                 if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
                     link = info.invite_link
                 self.invitelink = link
                 self.LOGGER(__name__).info(
-                    f"FORCE_SUB_CHANNEL1 detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                    f"FORCE_SUB_CHANNEL detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
                 )
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_CHANNEL1!"
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_CHANNEL!"
                 )
                 self.LOGGER(__name__).warning(
-                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL1}"
+                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
                     "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
