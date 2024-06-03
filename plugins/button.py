335bb595cfa209601fe71a6550f5328fd56e9153
Mikedromeda <82773161+Mikedromeda@users.noreply.github.com>
2024-06-04 00:57:34 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -85,7 +85,7 @@ def fsub_button(client, message):
     if not FORCE_SUB_CHANNEL1 and FORCE_SUB_CHANNEL2 and not FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="ɢᴄ sᴄғ", url=client.invitelink2),
+                InlineKeyboardButton(text="ᴄʜ sʀᴀ", url=client.invitelink2),
             ],
         ]
         try:
@@ -103,7 +103,7 @@ def fsub_button(client, message):
     if not FORCE_SUB_CHANNEL1 and not FORCE_SUB_CHANNEL2 and FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="ɢᴄ ᴀɴᴏɴ", url=client.invitelink3),
+                InlineKeyboardButton(text="ɢᴄ sᴄғ", url=client.invitelink3),
             ],
         ]
         try:
